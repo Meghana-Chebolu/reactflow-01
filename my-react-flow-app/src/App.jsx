2d1f4cec1ea6fbce72809e223b23da78d7a57ce8
Meghana Chebolu <meghanachebolu@gmail.com>
2023-11-13 10:14:06 +0000

    Image processing-37 example

--- my-react-flow-app/src/App.jsx
+++ my-react-flow-app/src/App.jsx
@@ -12,11 +12,18 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 
 const initialNodes = [
-  { id: '1',sourcePosition: 'right', position: { x: 50, y: 100 }, data: { label: 'Start' } },
-  { id: '2',sourcePosition: 'right',targetPosition: 'left', position: { x: 300, y: 100 }, data: { label: 'get_pdf' } },
-  { id: '3',sourcePosition: 'right',targetPosition: 'left', position: { x: 550, y: 100 }, data: { label: 'page-37' } },
-  { id: '4',sourcePosition: 'right',targetPosition: 'left', position: { x: 800, y: 100 }, data: { label: 'image_processing-37' } },  
-
+  { id: '1',sourcePosition: 'right', position: { x: 20, y: 200 }, data: { label: 'Start' } },
+  { id: '2',sourcePosition: 'right',targetPosition: 'left', position: { x: 300, y: 200 }, data: { label: 'get_pdf' } },
+  { id: '3',sourcePosition: 'right',targetPosition: 'left', position: { x: 550, y: 200 }, data: { label: 'page-37' } },
+  { id: '4',sourcePosition: 'right',targetPosition: 'left', position: { x: 800, y: 200 }, data: { label: 'image_processing-37' } },  
+  { id: '5',sourcePosition: 'right',targetPosition: 'left', position: { x: 1050, y: 50 }, data: { label: 'paragraph_identification-37' } },  
+  { id: '6',sourcePosition: 'right',targetPosition: 'left', position: { x: 1050, y: 190 }, data: { label: 'header_identification-37' } },  
+  { id: '7',sourcePosition: 'right',targetPosition: 'left', position: { x: 1050, y: 350 }, data: { label: 'table_identification-37' } }, 
+  { id: '8',sourcePosition: 'right',targetPosition: 'left', position: { x: 1300, y: 50 }, data: { label: 'paragraph_extraction-37' } },  
+  { id: '9',sourcePosition: 'right',targetPosition: 'left', position: { x: 1300, y: 197 }, data: { label: 'header_extraction-37' } },  
+  { id: '10',sourcePosition: 'right',targetPosition: 'left', position: { x: 1300 , y: 350 }, data: { label: 'table_extraction-37' } },  
+  { id: '11',sourcePosition: 'right',targetPosition: 'left', position: { x: 1550 , y: 197 }, data: { label: 'combine-37' } },  
+  { id: '12',sourcePosition: 'right',targetPosition: 'left', position: { x: 1800 , y: 197 }, data: { label: 'total_page_combine' } },  
 ];
 const initialEdges = [
   {
@@ -45,7 +52,111 @@ const initialEdges = [
     targetPosition: 'left', // Specify the target position to 'left'
     markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
 
-  },  
+  },
+  {
+    id: 'e4-5',
+    source: '4',
+    target: '5',
+    sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left', // Specify the target position to 'left'
+    type: 'smoothstep',
+    animated: true,
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  }, 
+  {
+    id: 'e4-6',
+    source: '4',
+    target: '6',
+    sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left',
+    type: 'smoothstep',
+    animated: true, // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  },
+  {
+    id: 'e4-7',
+    source: '4',
+    target: '7',
+    sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left',
+    type: 'smoothstep',
+    animated: true, // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  },
+  {
+    id: 'e5-8',
+    source: '5',
+    target: '8',
+        sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left', // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  },
+  {
+    id: 'e6-9',
+    source: '6',
+    target: '9',
+        sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left', // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  }, 
+  {
+    id: 'e7-10',
+    source: '7',
+    target: '10',
+        sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left', // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  },
+  {
+    id: 'e8-11',
+    source: '8',
+    target: '11',
+        sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left',
+    type: 'smoothstep',
+    animated: true, // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  }, 
+  {
+    id: 'e9-11',
+    source: '9',
+    target: '11',
+        sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left',
+    type: 'smoothstep',
+    animated: true, // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  },
+  {
+
+    id: 'e10-11',
+    source: '10',
+    target: '11',
+        sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left',
+    type: 'smoothstep',
+    animated: true, // Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  },
+  {
+    
+    id: 'e11-12',
+    source: '11',
+    target: '12',
+        sourcePosition: 'right', // Specify the source position to 'right'
+    targetPosition: 'left',// Specify the target position to 'left'
+    markerEnd: { type: MarkerType.ArrowClosed, width: 20, height: 20, color: '#000000' },
+
+  },
 ];
 
 export default function App() {
